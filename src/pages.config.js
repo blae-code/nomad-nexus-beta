a5b0cedddbb3a7429dc8cdf61c05d2195edce83c
Blae Hansen <blae@katrasoluta.com>
2025-12-02 22:43:31 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import Event from './pages/Event';
 import Events from './pages/Events';
 import Channels from './pages/Channels';
-import OpsDashboard from './pages/OpsDashboard';
 import Treasury from './pages/Treasury';
 import CommsConsole from './pages/CommsConsole';
 import NomadOpsDashboard from './pages/NomadOpsDashboard';
@@ -13,7 +12,6 @@ export const PAGES = {
     "Event": Event,
     "Events": Events,
     "Channels": Channels,
-    "OpsDashboard": OpsDashboard,
     "Treasury": Treasury,
     "CommsConsole": CommsConsole,
     "NomadOpsDashboard": NomadOpsDashboard,
