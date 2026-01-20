988978a2070236832ebd538ae30890b0c26e034b
Blae Hansen <blae@katrasoluta.com>
2026-01-20 22:22:55 +0000

    File changes

--- src/components/hooks/useVoiceAudio.jsx
+++ src/components/hooks/useVoiceAudio.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import { useLocalParticipant } from 'livekit-react';
 
 export function useVoiceAudio() {
   const [micEnabled, setMicEnabled] = useState(false);
