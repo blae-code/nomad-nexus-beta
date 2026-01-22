a0dd6c0407472c89e8750936e415af85fc58dbde
Blae Hansen <blae@katrasoluta.com>
2026-01-22 21:59:56 +0000

    File changes

--- src/pages/Hub.jsx
+++ src/pages/Hub.jsx
@@ -192,9 +192,9 @@ export default function HubPage() {
               </div>
               </div>
               </motion.div>
-              </motion.div>
+              </div>
 
-            {/* Live Operational Pulse */}
+              {/* Live Operational Pulse */}
         {recentLogs.length > 0 && (
           <motion.div
             initial={{ opacity: 0, y: 8 }}
