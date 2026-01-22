97b7866b9277021e07b1343fba4f7b7956ce9b6d
Blae Hansen <blae@katrasoluta.com>
2026-01-22 22:46:01 +0000

    File changes

--- src/pages/Hub.jsx
+++ src/pages/Hub.jsx
@@ -188,6 +188,7 @@ export default function HubPage() {
                     />
                   </div>
                 </motion.div>
+              </div>
             </div>
             </motion.div>
           </div>
